2a6ca126d46eeaa3855e446fe2cb34e35819b47f
Sedki Ltifi <ltifisedkiinfo@gmail.com>
2020-05-12 20:07:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import TodoItems from "./components/TodoItems/TodoItems";
+import AddItem from "./components/addItem/AddItem";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    items: [],
+  };
+
+  deletItem = (id) => {
+    console.log(id);
+    let items = this.state.items; //hna daba 7atit dik array items f1 variable smito items
+    let i = items.findIndex((item) => item.id === id); //hna daba galt lih dir boucle 3la array b dik finIndex o ligne li l9ayti item.id dyalo === dak id li dawazna lih 7ato f1  variable i
+    items.splice(i, 1); //supprimer liya la ligne li l9iti
+    this.setState({ items });
+
+    // deuxiemme méthode de suppression
+    // f had méthode kandir fonction filter li katdoz liya 3la chaque ligne o katakhod liya l id dyal chaque ligne o bine insertion o katchof wach dak id ma kaysawich id li 3tito o kaykhaliya o kay7ayad li kaysawi dak id
+
+    // let items = this.state.items.filter(item => {
+    //   return item.id != id;
+    // })
+
+    // this.setState(items);
+  };
+
+  // visibleMembers() {
+  //   return this.items.filter(m => m.display)
+  // }
+
+  addItem = (item) => {
+    item.id = this.state.items.length + 1;
+    let items = this.state.items;
+    items.push(item);
+    this.setState({ items: items });
+  };
+
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="text-center">Tod list App</h1>
+        <p>Number of rows = {this.state.items.length}</p>
+        <TodoItems vals_array={this.state.items} deleteItem={this.deletItem} />
+        <AddItem addItem={this.addItem} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoItems/TodoItems.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './TodoItems.css'
+
+
+
+const TodoItems = (props) =>{   //(props) hadi kandirha wast fonction bach njib props dial had component(TodoItems) mn autre componenst(App.js)
+
+        const {vals_array} = props; //val_array le nom dyal props li kayn f l'autre component f had l'exempls 3andna howa App.js
+        const {deleteItem} = props;
+
+        let lengeur_array = vals_array.length;
+       
+            const  ListeItems = lengeur_array ? (
+            vals_array.map( val =>{
+                return (
+                    <div key={val.id}>
+                        <span className="field_class id_field" >{val.id}</span>
+                        <span className="field_class" >{val.name}</span>
+                        <span className="field_class" >{val.age}</span>
+                        {/* <span onClick={deleteItem(val.id)}>x</span>comme ca fonction ka it2éxécuta rasha pour éviter foction tdir execution tout seule kanzid arrow function */}
+                        <span  className="field_class id_field" onClick={()=>deleteItem(val.id)}>x</span> 
+                        {/* {()=>deleteItem(val.id)} Had kitaba kat3ni dart 1 fonction sans nom o bien renommage o hadik lfonction li sans nom ghada tdir liya run l fonction deleteItem*/}
+                    </div>
+                )
+
+            })
+            ):(
+                <p>There is no item to show</p>
+            )
+
+       
+       
+         console.log(props);
+
+    return (
+
+        <div className="liste_item_tale">
+            <div>
+                <span className="header">Id</span>
+                <span className="header">Name</span>
+                <span className="header">Age</span>
+                <span className="header">Action</span>
+            </div>
+
+            {ListeItems}
+        </div>
+
+    )
+
+}
+
+export default TodoItems;
+
+// 7na f had todoItems ma ghadich n7taj state docn ghadi ndir fonction UnlessState
